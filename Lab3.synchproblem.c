3022ff5e66b30ac06f0578c479630ecdc31b43eb
Mark Jia <mjia@ualberta.ca>
2022-10-06 18:48:49 -0600

    add design doc

---
+++ Lab3.synchproblem.c
@@ -0,0 +1,20 @@
+/*
+# NAME: Matthew Munro
+# NSID: mam552
+# STUDENT NUMBER: 11291769
+# NAME: Yi Luan
+# NSID: yil160
+# STUDENT NUMBER: 11253856
+# NAME: Mark Jia
+# NSID: mij623
+# STUDENT NUMBER: 11271998
+# CMPT 332 2022
+# Lab3
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+

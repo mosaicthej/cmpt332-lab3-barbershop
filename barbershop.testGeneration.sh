f8d869586b7eacf4ab58868bfc5a128e9a9bcd61
Mark Jia <mjia@ualberta.ca>
2022-10-11 21:14:25 -0600

    Update and rename Lab3.synchproblem.testGeneration.sh to barbershop.testGeneration.sh

--- barbershop.testGeneration.sh
+++ barbershop.testGeneration.sh
@@ -14,9 +14,9 @@ do
     MaxCustomers=$(( (RANDOM % 40) + 10))
     numChairs=$(( (RANDOM % 7) + 3))
     echo -e "Test Case $i : MaxCustomers = $MaxCustomers, numChairs = $numChairs" >> Lab3.test.results.txt
-    ./Lab3 $MaxCustomers $numChairs >> Lab3.test.results.txt
+    ./barbershop $MaxCustomers $numChairs >> barbershop.test.results.txt
     # append 3 new lines between each test case
-    echo -e "\n\n\n" >> Lab3.test.results.txt   
+    echo -e "\n\n\n" >> barbershop.test.results.txt   
 done
 
 

b692cb363fc970ba113f84888f53a65ca5aede97
Mark Jia <mjia@ualberta.ca>
2022-10-11 21:13:06 -0600

    Update and rename Lab3.synchproblem.c to barbershop.c

--- barbershop.c
+++ barbershop.c
@@ -1,18 +1,4 @@
 /*
-# NAME: Matthew Munro
-# NSID: mam552
-# STUDENT NUMBER: 11291769
-# NAME: Yi Luan
-# NSID: yil160
-# STUDENT NUMBER: 11253856
-# NAME: Mark Jia
-# NSID: mij623
-# STUDENT NUMBER: 11271998
-# CMPT 332 2022
-# Lab3
-*/
-
-/*
     simulation of the barber shop problem
     designs are layed out in the Lab3.synchproblem.txt file
 */
